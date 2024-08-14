fffc665f9998c828cbccb4bcf11f3affc3b6f027
Shivsharan yadav <shivsharanyadav9516@gmail.com>
2024-08-14 11:54:52 +0530

    Fetch

--- promises.js
+++ promises.js
@@ -1,70 +1,103 @@
-const promiseOne = new Promise( function( resolve , reject){
-    //Do adync tast
-    setTimeout(function(){
-        console.log('Aync tast is completed');
-        resolve()
+// const promiseOne = new Promise( function( resolve , reject){
+//     //Do adync tast
+//     setTimeout(function(){
+//         console.log('Aync tast is completed');
+//         resolve()
         
-    }, 1000)
-}) 
+//     }, 1000)
+// }) 
 
-promiseOne.then(function(){
-    console.log('promise consume');
+// promiseOne.then(function(){
+//     console.log('promise consume');
     
-})
+// })
 
 
 
-new Promise(function(resolve,reject){
-    setTimeout(function(){
-        console.log('Aync task');
-        resolve()
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log('Aync task');
+//         resolve()
         
-    })
-}).then(function(){
-    console.log('aync to resolve');
+//     })
+// }).then(function(){
+//     console.log('aync to resolve');
     
-})
+// })
 
 
-3rd 
+// 3rd 
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username:"shivsharan ",email: "shivsharanyadav9516@gmail.com"},2000)
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username:"shivsharan ",email: "shivsharanyadav9516@gmail.com"},2000)
 
-    })
-})
-promiseThree.then(function(user){
-    console.log(user);
+//     })
+// })
+// promiseThree.then(function(user){
+//     console.log(user);
     
-})
+// })
 
-promise 4
-const promiseFour = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error = true
-        if(!error){
-            resolve({username:"Alok",password:"Shiv@2002"})
-        }
-        else{
-            reject('ERROR: something went wrong')
-        }
-    })
-})
-promiseFour
-.then((user) => {
-    console.log(user);
-    return user.username
+// promise 4
+// const promiseFour = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = true
+//         if(!error){
+//             resolve({username:"Alok",password:"Shiv@2002"})
+//         }
+//         else{
+//             reject('ERROR: something went wrong')
+//         }
+//     })
+// })
+// promiseFour
+// .then((user) => {
+//     console.log(user);
+//     return user.username
     
-})
-.then((username) => {
+// })
+// .then((username) => {
 
-    console.log(username);
+//     console.log(username);
     
 
-})
-.catch(function(error){
-    console.log(error);
+// })
+// .catch(function(error){
+//     console.log(error);
     
-})
-.finally(() => console.log("promishesh either resoloveor rejected"))
+// })
+// .finally(() => console.log("promishesh either resoloveor rejected"))
+
+
+
+// async function getAllUser() {
+//     try {
+//         const response =  await  fetch('https://jsonplaceholder.typicode.com/posts')
+
+//         const data =  await response.json()
+//        console.log(data);
+
+
+
+//     }
+//     catch (error){
+//         console.log("E:",error);
+        
+//     }
+    
+      
+// }
+// getAllUser()
+
+
+// fetch ('https://jsonplaceholder.typicode.com/posts')
+// .then((response)=> {
+//     return response.json()
+// })
+// .then((data) => {
+//     console.log(data);
+    
+
+// })
+// .catch((error) => console.log(error))
